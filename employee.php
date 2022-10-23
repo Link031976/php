51ae07b2fb20045339a2ab59197684af3f935e6b
Alex <link_igor@i.ua>
2022-10-23 13:30:36 +0300

    lesson4

---
+++ employee.php
@@ -0,0 +1,31 @@
+<?php
+class Employee
+    {
+        public $name;//Ім'я
+        public $age ;//Вік
+        public $salary;//зарплата
+
+        public function __construct($a,$b,$c) //конструктор
+        {
+            $this->name=$a;
+            $this->age=$b;
+            $this->salary=$c;
+        }
+        public function getName() //повернення ім'я
+            {echo "Працівник ".$this->name;}
+
+        public function getAge() //повернення віку
+            {return $this->age;}
+
+        public function getSalary() // повернення рівня зп
+            {return $this->salary;}
+
+        public function checkAge() //перевірка на повноліття
+            {
+                if ($this->$age>18) return TRUE;
+                   else return FALSE;
+            }
+
+        public function doubleSalary()
+            {$this->salary=$this->salary*2;}
+    }
