30294db1f2777db86601e34be8ad0b8d370ac5b5
Alex <link_igor@i.ua>
2022-11-04 12:29:54 +0200

    1

--- employee.php
+++ employee.php
@@ -29,3 +29,21 @@ class Employee
         public function doubleSalary()
             {$this->salary=$this->salary*2;}
     }
+    class Arr
+    {
+        private $numbers = []; // масив чисел
+        public function __construct($numbers)
+        {
+            $this->numbers = $numbers; // записуємо масив $numbers у властивість
+        }
+        // Додаємо ще одне число до нашого масиву:
+        public function add($number)
+        {
+            $this->numbers[] = $number;
+        }
+        // Знаходимо суму чисел:
+        public function getSum()
+        {
+            return array_sum($this->numbers);
+        }
+    }
--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
-require_once('employee.php');
 
-$user1=new Employee('John',25,1000); //створення об'єкту work1 класу Employee
-$user2=new Employee('urik',26,2000); //створення об'єкту work2 класу Employee
+require_once('employee.php');
+require_once('arr.php');
+include ('arr.php');
+
+$user1 = new Employee ('John',25,1000); //створення об'єкту work1 класу Employee
+$user2 = new Employee ('urik',26,2000); //створення об'єкту work2 класу Employee
         
 echo "Загальна зарплата ".$user1->getSalary()+$user2->getSalary()."<br>";
 echo "Сумарний вік ".$user1->getAge()+$user2->getAge()."<br>";
+
+$arr = new Arr ([1, 2, 3]); // створюємо об'єкт, записуємо до нього масив [1, 2, 3]
+$arr->add(4); // Додаємо в кінець масиву число 4
+$arr->add(5); // додаємо в кінець масиву число 5
+// Знаходимо суму елементів масиву:
+echo $arr->getSum(); // виведе 15
