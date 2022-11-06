812b48115e60e2bdf228ce1260e3d4dafcb9c9a7
Alex <link_igor@i.ua>
2022-11-06 21:13:22 +0200

    level 1

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
-require_once('employee.php');
+    $file='c:\web';
+    class Bilder 
+        {
+            public $dir=[];          
 
-$user1=new Employee('John',25,1000); //створення об'єкту work1 класу Employee
-$user2=new Employee('urik',26,2000); //створення об'єкту work2 класу Employee
-        
-echo "Загальна зарплата ".$user1->getSalary()+$user2->getSalary()."<br>";
-echo "Сумарний вік ".$user1->getAge()+$user2->getAge()."<br>";
+            public function getDir($f)
+            {
+                $dir=[];
+                $bf = new DirectoryIterator($f);
+                foreach ($bf as $fileinfo) 
+                {                    
+                    if ( $fileinfo->getType() == 'dir') 
+                        if (($fileinfo->getFilename() != '.') or ($fileinfo->getFilename() != "..")) $dir[0][] = $fileinfo->getFilename();
+                    if ( $fileinfo->getType() == 'file') $dir[1][] = $fileinfo->getFilename();
+                   // echo $fileinfo->getFilename() . " " . $fileinfo->getType() . "<br>";
+                    
+                }
+                return $dir;
+            }
+
+           
+        }
+
+    $b = new Bilder;
+    $s=$b->getDir($file);
+
+    echo "<pre>";
+    print_r($s);
+
+  
