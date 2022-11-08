45aea0e1563ce55ede70a468c1a5d8f7d9b23b71
Alex <link_igor@i.ua>
2022-11-08 21:56:09 +0200

    level 2.1

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
-    $f='C:\EFI\Microsoft\Boot'; 
+    $f='D:\Дом'; //Полный путь к корневой папке для построения дерева каталогов
+    
 
     function bildTree($f)
     {
-        $bf = new DirectoryIterator($f); //создаю объект методов для работы с файлами
-        foreach ($bf as $fileinfo) //перебираю объекты текущей папки
+        $bf = new DirectoryIterator($f); //создаю объект с методами для работы с файлами
+        foreach ($bf as $fileinfo) //перебираю объекты (файлі и папки) текущей папки
         {                    
             $name = $fileinfo->getFilename(); // имя файла/папки
             if ( $fileinfo->getType() == 'dir') // добавляю в список папок                   
-                if ($name != ".")
-                    if ($name != "..") 
+                if ($name != ".") // исключаю возврат в корневой каталог диска
+                    if ($name != "..") // исключаю возврат из текущей папки
                         {
                             echo "<li style='color: blue'>".$name;
                             echo "<ul >";
-                            $name=$f."\\".$name;    
-                            
-                                bildTree($name);                                
+                            $name=$f."\\".$name;
+                                bildTree($name); // рекурсивно вызывается данная функция                                
                             echo "</ul>";
                             echo"</li>";
                         }
@@ -23,7 +23,8 @@
             if ( $fileinfo->getType() == 'file') echo "<li style='color: green'>".$name."</li>"; //добавляю в список файлов                              
         }
     }
-    echo "Дерево папки ".$f."<br>";
+
+    echo "<h2 align='center'>Дерево папки ".$f."</h2><br>"; 
     echo "Умовні позначення: <span style='color: blue'>назва папки</span>, <span style='color: green'> 
         назва файлу </span></span><ul>";
     bildTree($f);
