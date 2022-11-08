3812e14dacc46de94e0b2a9e6ccdad0e1c46b158
Alex <link_igor@i.ua>
2022-11-08 21:41:09 +0200

    level 2

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?php
-    $file='c:\web';
-    class Bilder 
-        {
-            public $dir=[];          
+    $f='C:\EFI\Microsoft\Boot'; 
 
-            public function getDir($f)
-            {
-                $dir=[];
-                $bf = new DirectoryIterator($f);
-                foreach ($bf as $fileinfo) 
-                {                    
-                    if ( $fileinfo->getType() == 'dir') 
-                        if (($fileinfo->getFilename() != '.') or ($fileinfo->getFilename() != "..")) $dir[0][] = $fileinfo->getFilename();
-                    if ( $fileinfo->getType() == 'file') $dir[1][] = $fileinfo->getFilename();
-                   // echo $fileinfo->getFilename() . " " . $fileinfo->getType() . "<br>";
-                    
-                }
-                return $dir;
-            }
-
-           
+    function bildTree($f)
+    {
+        $bf = new DirectoryIterator($f); //создаю объект методов для работы с файлами
+        foreach ($bf as $fileinfo) //перебираю объекты текущей папки
+        {                    
+            $name = $fileinfo->getFilename(); // имя файла/папки
+            if ( $fileinfo->getType() == 'dir') // добавляю в список папок                   
+                if ($name != ".")
+                    if ($name != "..") 
+                        {
+                            echo "<li style='color: blue'>".$name;
+                            echo "<ul >";
+                            $name=$f."\\".$name;    
+                            
+                                bildTree($name);                                
+                            echo "</ul>";
+                            echo"</li>";
+                        }
+              
+            if ( $fileinfo->getType() == 'file') echo "<li style='color: green'>".$name."</li>"; //добавляю в список файлов                              
         }
+    }
+    echo "Дерево папки ".$f."<br>";
+    echo "Умовні позначення: <span style='color: blue'>назва папки</span>, <span style='color: green'> 
+        назва файлу </span></span><ul>";
+    bildTree($f);
+    echo "</ul>";
+    
+    
 
-    $b = new Bilder;
-    $s=$b->getDir($file);
-
-    echo "<pre>";
-    print_r($s);
 
   
