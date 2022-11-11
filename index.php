00e8d9d2a82cf0f526d226e7fba5726cfae80bb0
Alex <link_igor@i.ua>
2022-11-11 19:53:30 +0200

    v 1.0

--- index.php
+++ index.php
@@ -1,8 +1,82 @@
 <?php
-require_once('employee.php');
+    class User
+    {
+        protected $name;
+        private   $age;
+        private   $surname;
+        private   $birthday;
 
-$user1=new Employee('John',25,1000); //створення об'єкту work1 класу Employee
-$user2=new Employee('urik',26,2000); //створення об'єкту work2 класу Employee
-        
-echo "Загальна зарплата ".$user1->getSalary()+$user2->getSalary()."<br>";
-echo "Сумарний вік ".$user1->getAge()+$user2->getAge()."<br>";
+        public function __construct($name,$surname,$birthday)
+        {
+            $this->name = $name;
+            $this->surname = $surname;
+            $this->birthday = $birthday;
+            $this->age = $this->calculateAge();
+        }
+
+       private function calculateAge()
+        {
+            return floor((strtotime(date('Y-m-d')) - strtotime(date($this->birthday))) / 86400 / 365); 
+        }
+        public function getName()
+        {
+            return $this->name;
+        }
+        public function getBirthday()
+        {
+            return $this->birthday;
+        }
+        public function getSurname()
+        {
+            return $this->surname;
+        }
+        public function setName($name)
+        {
+            if (strlen($name) > 3) $this->name = $name;
+        }
+        public function getAge()
+        {
+            return $this->age;
+        }
+        public function setAge($age)
+        {
+            if ($age >= 18)
+            {
+                $this->age = $age;
+            }
+        }
+    }
+    class Employee extends User
+    {
+        public $salary;
+
+        public function __construct($name , $surname , $birthday , $salary)
+        {
+            parent::__construct($name,$surname,$birthday);
+            $this->salary = $salary;
+        }
+        public  function getSalary()
+        {
+            return $this->salary;
+        }
+    }
+
+    class Product
+    {
+        public $name;
+        public $price;
+    }
+    
+    $user = new User('Jon','Look','1992-02-03');
+    $employee = new Employee('Jon','Look','1987-02-03',15000);
+    $product1 =  new Product;
+
+    echo "<pre>";
+    print_r($user);
+    print_r($employee);
+
+    print_r($product1);
+    $product2 = $product1;
+    $product2->name = 'Jon';
+    print_r($product1);
+    print_r($product2);
