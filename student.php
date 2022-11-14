7bd9dabf9e73b926d954321a209d775ec6240e22
Alex <115179267+Link031976@users.noreply.github.com>
2022-11-14 21:08:13 +0200

    Update student.php

--- student.php
+++ student.php
@@ -1,4 +1,9 @@
 <?php
-    class Student extends User
-    {
-    }
+       class Student extends User
+      {
+          public function setName($name)
+          {
+              //Task 2. перевірка довжени імені. Якщо меньше 10, то "батько" первить решту
+              if (strlen($name) < 10) parent::setName($name);
+          }
+      }
