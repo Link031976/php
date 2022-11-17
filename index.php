0fd83a8e536dd29ed0a3be8db0f8518f4b92b7d0
Alex <link_igor@i.ua>
2022-11-17 10:36:46 +0200

    Task7 vb1

--- index.php
+++ index.php
@@ -3,6 +3,34 @@
     require_once('sumHelper.php');
     require_once('avgHelper.php');
 
+    class Product
+    {
+        private readonly string $name;
+        private readonly int $price;
+        private readonly int $quantity;
+
+        public function __construct($name,$price,$quantity)
+        {
+            $this->name = $name;
+            $this->price = $price;
+            $this->quantity = $quantity;
+        }
+        //Task 6
+        public function getCost()
+        {
+            return $this->price * $this->quantity;
+        }
+    }
+
+    class Cart
+    {
+        public $product;
+
+        public function add($products)
+        {
+            $this->product = $products;
+        }
+    }
     $mas = [1,2,3,4,5];
     $avgHelper = new AvgHelper;
     echo "Для масиву <pre>";
@@ -18,4 +46,11 @@
     $arr->add(12);
     echo "Для масиву Arr <pre>";
     print_r($arr);
-    echo "сума середнього арифметичного та середнього квадратичного масиву ".$arr->getAvgMeanSum();
+    echo "сума середнього арифметичного та середнього квадратичного масиву ".$arr->getAvgMeanSum()."<br>";
+        //Task 5
+    $product = new Product('шкарпетки',5,10);
+    //Task 6
+    echo "Вартість продукту ".$product->getCost()."<br>";
+    //Task 7
+    $cart = new Cart;
+    $cart->add()
