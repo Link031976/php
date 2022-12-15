81dda67f08d0e9d6688c3fdc63004e390850cea8
Alex <link_igor@i.ua>
2022-12-15 13:18:59 +0200

    lesson18 completing tasks in the lesson

--- index.php
+++ index.php
@@ -1,2 +1,59 @@
 <?php
-    require_once('autoloader.php');
+//$f='';
+//require_once('autoloader.php');
+echo "<a href='index_in_lesson17.php'>Перейти до завдання з уроку 17</a>";
+interface Figure
+{
+    public function getSquare();
+    public function getPerimeter();
+}
+class Quadrate implements Figure
+{
+    private $a;
+    public function __construct($a)
+    {
+        $this->a = $a;
+    }
+    public function getSquare()
+    {
+        return $this->a * $this->a;
+    }
+    public function getPerimeter()
+    {
+        return 4 * $this->a;
+    }
+}
+
+class Rectangle implements Figure
+{
+private $a;
+private $b;
+public function __construct($a, $b)
+{
+$this->a = $a;
+$this->b = $b;
+}
+public function getSquare()
+{
+return $this->a * $this->b;
+}
+public function getPerimeter()
+{
+return 2 * ($this->a + $this->b);
+}
+}
+
+
+
+
+class MyClass
+{
+}
+
+class NotMyClass
+{
+}
+$a = new MyClass;
+
+var_dump($a instanceof MyClass);
+var_dump($a instanceof NotMyClass);
---
+++ index_in_lesson17.php
@@ -0,0 +1,19 @@
+<?php
+echo "<a href='index.php'>Повернутися до виконання д/з до уроку 17</a>";
+
+
+//in interface_exists
+//Нехай у вас є інтерфейс iTest1 і немає інтерфейсу iTest2.
+//Перевірте, чи виведе функція interface_exists для інтерфейсу iTest1 та для інтерфейсу iTest2
+echo "<h4 style='color: green'>interface_exists</h4>";
+
+interface iTest1 
+{}
+
+if (interface_exists('iTest1')) echo " interface iTest1  - exists<br>"; else  echo " interface iTest1  - not exists<br>";
+if (interface_exists('iTest2')) echo " interface iTest2  - exists<br>"; else  echo " interface iTest2  - not exists<br>";
+
+echo "<h4 style='color: green'>get_declared_interfaces</h4>";
+//Виведіть список усіх оголошених інтерфейсів.
+echo "<pre>";
+print_r(get_declared_interfaces());
